9280326f26cf756ece01caccc39067d59485a9e0
C-Schweiger <90523452+C-Schweiger@users.noreply.github.com>
2022-01-24 19:17:45 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Corey Schweiger Home Page</title>
+<style>
+body {
+	background-image: url("lab1_background.jpg");
+	background-repeat: no-repeat;
+	
+}
+</style>
+</head>
+
+<body>
+<h1>Welcome to my page!</h1>
+<p>Name:<b>Corey Schweiger</b></p>
+<p>Major:<span style="color:red"> Computer Science</span></p>
+<p>Title:<span style="font-family:arial"> Graduate Student</span><br></p>
+<p>Hobby:<span style="font-family:arial;color:blue;"><i> Videogames</i></span></p>
+<p>Facebook:<a href="https://www.facebook.com">here</a>
+<h2>My favorite movie star: Leonardo Dicaprio</h2>
+<img src="leonardo_dicaprio.jpg">
+</body>
+</html>
